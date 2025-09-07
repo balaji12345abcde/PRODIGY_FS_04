280286b723ff54d23e99e93db18060569e190801
balaji12345abcde <balaji405791@gmail.com>
2025-09-07 19:06:46 +0530

    Add files via upload

---
+++ main/chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    Room_name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.Room_name
+class Messages(models.Model):
+    room=models.ForeignKey(Room,on_delete=models.CASCADE)
+    sender=models.CharField(max_length=50)
+    message=models.TextField()
+    #time_stamp=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{str(self.room)}-{self.sender}"
